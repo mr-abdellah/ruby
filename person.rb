f0f1c694453041c979bd329313d80247f46cbf67
mr-abdellah <belkaid.abdulah@gmail.com>
2022-09-20 15:39:08 +0100

    add decoratot

--- person.rb
+++ person.rb
@@ -1,4 +1,7 @@
-class Person
+require_relative 'nameable'
+require_relative 'trimmer_decorator'
+require_relative 'capitalize_decorator'
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
@@ -13,9 +16,20 @@ class Person
     of_age? || @parent_permission ? true : false
   end
 
+  def correct_name
+    @name
+  end
+
   private
 
   def of_age?
     @age >= 18
   end
 end
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+capitalizedPerson = CapitalizeDecorator.new(person)
+puts capitalizedPerson.correct_name
+capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+puts capitalizedTrimmedPerson.correct_name
